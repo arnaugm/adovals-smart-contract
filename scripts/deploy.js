741bde1119fbfb94c4ec599e0f17afb7c0894388
Arnau González <arnau.gonzalez@skyscanner.net>
2022-06-02 11:28:06 +0200

    merkle tree validation in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,37 @@
 /* eslint-disable no-console */
+
 const { ethers } = require('hardhat');
+const keccak256 = require('keccak256');
+const { MerkleTree } = require('merkletreejs');
+
+const whitelist = require('./whitelist.json');
+
+const generateMerkleTree = (addresses) => {
+  const leaves = addresses.map((addr) => keccak256(addr));
+
+  const merkleTree = new MerkleTree(leaves, keccak256, {
+    sortPairs: true,
+  });
+
+  return `0x${merkleTree.getRoot().toString('hex')}`;
+};
 
 async function main() {
   const Adovals = await ethers.getContractFactory('Adovals');
+
+  const merkleRoot = generateMerkleTree(whitelist);
 
   // Start deployment, returning a promise that resolves to a contract object
   const adovals = await Adovals.deploy(
     'Adovals',
     'ADV',
     'ipf://base-url.com/',
-    '0xa171a3d6781c78d78bab31e28cd2988c7cd3e620b473cb903a5afbcd65d552dd',
+    merkleRoot,
   );
   await adovals.deployed();
-  console.log('Contract deployed to address:', adovals.address);
+  console.log(`Contract deployed to address: ${adovals.address}`);
+  console.log(`Whitelist: ${whitelist}`);
+  console.log(`Merkle root: ${merkleRoot}`);
 }
 
 main()
