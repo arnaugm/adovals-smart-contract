e806cfef09f808d302c0f3fbe881daad238d97de
Arnau González <arnau.gonzalez@skyscanner.net>
2022-10-19 21:47:45 +0200

    add config for prod deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,14 @@ require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-etherscan');
 require('hardhat-gas-reporter');
 
-const { PRIVATE_KEY, ETHERSCAN_API_KEY, GOERLI_API_URL, REPORT_GAS } =
-  process.env;
+const {
+  PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+  MAINNET_API_URL,
+  LEDGER_MNEMONIC,
+  GOERLI_API_URL,
+  REPORT_GAS,
+} = process.env;
 
 module.exports = {
   solidity: {
@@ -28,10 +34,21 @@ module.exports = {
       url: GOERLI_API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+    mainnet: {
+      url: MAINNET_API_URL,
+      accounts: {
+        mnemonic: LEDGER_MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: '',
+      },
+    },
   },
   etherscan: {
     apiKey: {
       goerli: ETHERSCAN_API_KEY,
+      mainnet: ETHERSCAN_API_KEY,
     },
   },
   gasReporter: {
